dfdf3f53688e43fd1205373bda48272d6539780f
Nkoech123 <nicholask320@gmail.com>
2022-06-28 10:52:00 -0700

    PFCard

--- React/portfolio/src/stories/PFCard/PFCard.stories.tsx
+++ React/portfolio/src/stories/PFCard/PFCard.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PFCardWithImage from '../../PFCard/PFCard';
+import PFCard from '../../PFCard/PFCard';
 
 import {HeadingProject1} from                        
 
@@ -12,7 +12,7 @@ export default {
 
 
   export const PFCardProject1 = () => {
-    return <PFCardWithImage
+    return <PFCard
 
             img= "https://s3-alpha-sig.figma.com/img/72e8/06d5/f2f83356a84d8c6504138fd80086491d?Expires=1657497600&Signature=OXwxsmwhIuAOPsLlGbxbpJCm0BQWYGOIKWgDfsIoA0PRluvbGzLedfIa7embFJZEKhJQRoaQH1JuWIOxkZ~fvIFiilL1kcHXU9W-bqlBqfLM~uCup1mSIhr00s7Hh9Ttj2VbRHJtPt3Vg59sk1beItG2NZaoq7826GLQVCmJBe6dymBKdBo0NvIkIy7Y5azsqv7jWeBKGzcQ9mOqQOI7FcNvygZCFvWUG5bvgFSXtH2p2BbwPSEgEKSpKQN1j86A4Rqu44qeBVrjNNZkuu1H0V70RK~hJvhS9g0GE9ii7W76INEWb2Vq2RuQ5bKqdg1DLgZqOzvfAd4QOYH0QFh1Jg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
             projectname="Project1"
