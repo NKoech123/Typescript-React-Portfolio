3edf63e7b6f471c7245180933709eb329b925014
Nkoech123 <nicholask320@gmail.com>
2022-07-02 02:40:46 -0700

    navbar styled

--- Typescript-ReactPortfolio/portfolio/src/components/header.tsx
+++ Typescript-ReactPortfolio/portfolio/src/components/header.tsx
@@ -3,26 +3,30 @@ import { Flex, Heading, HStack, Link,Text } from "@chakra-ui/react";
 
 function Header() {
   return (
-    <Flex px="200px" 
-    py="20px" 
-    width="full"  
+    <Flex bg="#1A2C32" width="full" height="65px">
+    <Flex mx="144px" 
+    mt="50px"
     bg="#1A2C32"
-    alignItems="flex-end"
+    alignItems="center"
     justifyContent="space-between">
         <Flex justifyContent="space-between">
             <Heading color="whiteAlpha.900"
-                    mr="900px"
-                    fontSize={20}
+                    ml="70px"
+                    fontSize={30}
+                    fontWeight="700"
+        
+                    height="45px"
                     letterSpacing="1.5px"
                 >
                     NK.
             </Heading>
-            <HStack color="whiteAlpha.900" spacing="40px">
-                <Link>Projects</Link>
-                <Link>Contacts</Link>
+            <HStack color="whiteAlpha.900" spacing="40px" ml="900px">
+                <Link fontSize={20} height={30} fontWeight={700}>Projects</Link>
+                <Link fontSize={20} height={30} fontWeight={700}>Contacts</Link>
                 
             </HStack>
         </Flex>
+    </Flex>
     </Flex>
   )
 }
