86deaa17aec68bc2b7752f04256736ebb142ad8a
Nkoech123 <nicholask320@gmail.com>
2022-07-05 16:14:41 -0700

    experiences

--- Typescript-ReactPortfolio/portfolio/src/pages/landing/landing.tsx
+++ Typescript-ReactPortfolio/portfolio/src/pages/landing/landing.tsx
@@ -3,12 +3,14 @@ import { Intro } from "./sections/intro";
 import { AboutMe } from './sections/aboutMe';
 import { Experience } from './sections/experience';
 import {Box} from "@chakra-ui/react";
+import {Projects} from './sections/projects'
 function Landing() {
   return (
     <Box>
         <Intro/>
         <AboutMe/>
         <Experience/>
+        <Projects/>
 
     </Box>
     
---
+++ Typescript-ReactPortfolio/portfolio/src/pages/landing/sections/projects.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PFCard from '../../../foundation/PFCard/PFCard'
+
+export const Projects = () => {
+  return (
+    
+  )
+}
+
