de32fb1eab9d6ed671b1040f4e8190a2ec462a4b
Nkoech123 <nicholask320@gmail.com>
2022-07-06 15:19:22 -0700

    get in touch

--- Typescript-ReactPortfolio/portfolio/src/pages/landing/sections/footer.tsx
+++ Typescript-ReactPortfolio/portfolio/src/pages/landing/sections/footer.tsx
@@ -1,30 +1,31 @@
 import React from 'react'
-import {Text,Flex, VStack, HStack} from '@chakra-ui/react'
+import {Text,Flex, HStack} from '@chakra-ui/react'
 import { SocialIcons } from '../../../components/socialIcons';
 
 export const Footer = () => {
   return (
-    <Flex w='100vw' h='100vh'>
+ 
+            <HStack   mx='8.33%' mt='14.68%' mb='8%'  justifyContent='center' alignContent='space between' >
 
-        <Flex mx='8.33%' mt='14.68%' mb='8%'>
-
-            <HStack  alignItems='center' justifyContent='start'>
-                <VStack>
-                    <Text>Texts</Text>
-                    <Text>Texts</Text>
-                    <Text>Texts</Text>
-                    <Text>Texts</Text>
-                </VStack>
-
-                <SocialIcons/>
                 
+                <Flex direction='column' mb='12px'>
+                    <Text textAlign='center'    color="rgba(70, 158, 132, 1)">What now?</Text>
+                    <Text fontSize='90px' fontWeight='700'>Get in touch</Text>
+                    <Text textAlign='start' mb='20px'>My name is Nicholas and I enjoy solving problems. <br/> My interest in coding started back in 2017 when I <br/> took CS61A — which introduced me to programing in <br/> python!</Text>
+                    <Text textAlign='start'>#BayArea #Fulltack #DataScience</Text>
+                </Flex>
+
+                <Flex justifyContent='center' ml='300px'>
+                   <SocialIcons/>
+                </Flex>
+              
 
             </HStack>
 
 
-        </Flex>
 
-    </Flex>
+
+
     
   )
 }
