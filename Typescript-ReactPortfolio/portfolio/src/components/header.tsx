639e24623caf26d5b6c3ba2185780675484f9b92
Nicholas Koech <nicholaskoech@Nicholass-MacBook-Pro.local>
2022-10-19 02:10:22 -0700

    small changes

--- Typescript-ReactPortfolio/portfolio/src/components/header.tsx
+++ Typescript-ReactPortfolio/portfolio/src/components/header.tsx
@@ -5,32 +5,38 @@ const Header = () => {
   return (
 
     <Flex mx="8.33%"
-        mt="4.47%"
-        mb="4.38%" 
-        h={["20.82%" ,"20.82%" ,"5.82%" ,"5.82%" ]}
-        justifyContent={['center','center','space-between','space-between']}
-        alignItems = {['center','center','space-between','space-between']}
-        direction={["column", "column", "row", "row"]}
-        gap='2.5rem'
+          mt="4.47%"
+          mb="4.38%" 
+          h={["20.82%" ,"20.82%" ,"5.82%" ,"5.82%" ]}
+          justifyContent={['center','center','space-between','space-between']}
+          alignItems = {['center','center','space-between','space-between']}
+          direction={["column", "column", "row", "row"]}
+          gap='2.5rem'
         >
-            <Flex ml={["4%","4%","4%","4%"]} >
-                <Flex fontSize='30px' fontWeight='900' color="whiteColor"> N<Text fontWeight='400'>K.</Text></Flex>
-            </Flex>
+       <Flex ml={["4%","4%","4%","4%"]} >
+          <Flex fontSize='30px' fontWeight='900'> N<Text fontWeight='400'>K.</Text></Flex>
+       </Flex>
 
-            <Flex  direction={["column", "column", "row", "row"]} 
-                  alignItems = {['center','center','space-between','space-between']}
-                  gap= {['2.5rem','2.5rem','0rem','0rem']}
-                  as='nav'
+      <Flex  direction={["column", "column", "row", "row"]} 
+             alignItems = {['center','center','space-between','space-between']}
+             gap= {['2.5rem','2.5rem','0rem','0rem']}
+             as='nav'
+             >
+              <Flex fontSize='20px' 
+                    fontWeight='600' 
+                    my={['10px','0px','0px','0px']} 
+                    >
+                      Projects
+              </Flex>
+              <Flex fontSize='20px' 
+                    fontWeight='600' 
+                    ml={['0px','80px','80px','80px']} 
+                    my={['10px','0px','0px','0px']}
                   >
-                    <Flex fontSize='20px' fontWeight='600' my={['10px','0px','0px','0px']} >
-                            <Text color="whiteColor">Projects</Text>
-                    </Flex>
-                    <Flex fontSize='20px' fontWeight='600' ml={['0px','80px','80px','80px']} my={['10px','0px','0px','0px']}>
-                          <Text color="whiteColor">Contact</Text>
-                    </Flex>
-            </Flex>
+                    Contact
+              </Flex>
+      </Flex>
        
-
     </Flex>
 
    
